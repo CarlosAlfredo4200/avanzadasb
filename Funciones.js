c8685fb3eb46310e0899f6adb79475dc5a4cab25
Carlos Alfredo Montoya Goez <CarlosMontoya@comfama.com.co>
2022-02-23 19:53:20 -0500

    V

--- Funciones.js
+++ Funciones.js
@@ -1,9 +1,22 @@
-import React from 'react'
+console.log("hola");
+const carro = {
+  placa: "FHE961",
+  modelo: "2009",
+  arrancar: function () {
+    console.log(`Encienda el carro ${this.placa} ${this.modelo}`);
+  },
+  parar: () => {
+    console.log(`Encienda el carro ${this.placa} ${this.modelo}`);
+  },
+};
+console.log(carro.arrancar());
 
-const Funciones = () => {
-  return (
-    <div>Funciones</div>
-  )
-}
-
-export default Funciones
+const producto = {
+  nombre: "Shampoo",
+  decirProducto: function () {
+    setTimeout(() => {
+      console.log("El nombre es : " + this.nombre);
+    }, 3000);
+  },
+};
+producto.decirProducto();
