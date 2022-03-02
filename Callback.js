00f053b9183505db4315cbe768c613a85e55658d
Carlos Alfredo Montoya Goez <CarlosMontoya@comfama.com.co>
2022-03-02 16:30:22 -0500

    Promesas

--- Callback.js
+++ Callback.js
@@ -1,5 +1,5 @@
-//callback es un argumento
-//funcion orden superior fn que llama a la otra atra vez del paramentro
+//callback es un argumento (Funcion utilizada como parametro en otra funcion)
+//funcion orden superior fn que llama a la otra atra vez del paramentro 
 
 function reflexion (frase, callback){
         const mensaje = 'Hierba mala, '+frase;
@@ -25,3 +25,36 @@ function calculo (n1,n2,callback){
     callback(suma)
 }
 calculo(5,7,myVista)
+
+//-------------------------------------------------
+
+function fncallback() {
+    console.log('Yo soy un callback');
+}
+
+function mensaje (fn) {
+    fn() ;
+}
+
+mensaje(fncallback)
+
+//-----------------------------------------------
+
+//Cuando lo utilizo
+
+function funcion1 (fn) {
+
+    setTimeout(() => {
+         
+        console.log('Mensaje uno');
+        fn()
+    }, 1000);
+}
+
+function funcion2 () {
+    console.log('Mensaje dos');
+
+}
+
+funcion1(funcion2)
+ 
--- Promesas.js
+++ Promesas.js
@@ -1,2 +1,103 @@
-//Promesas es algo que se espera then, catch
+//Promesas es algo que se utiliza para trabajar con peticiones asincronas y reducir el uso excesivo de callbak.  espera then, catch.
 
+// el método resolve esta relacionado directamente con then
+// el método reject esta relacionado directamente con catch
+
+let promesa = new Promise(function(resolve, reject){
+    let suma = 2 + 6 ;
+    
+    (suma === 8) ? resolve('Correcto!!!') : reject('Error!!!')
+
+})
+
+promesa.then(function(mensaje){
+    console.log(mensaje);
+}).catch(function(error){
+    console.log(error);
+    
+})
+
+
+//---------------------------------------
+
+function myPromesa(){
+    return new Promise(function(resolve, reject){
+        
+        let suma = 2 + 20 ;
+        (suma === 8) ? resolve('Correcto!!!') : reject('Error!!!')
+    
+    })
+}
+  
+
+myPromesa().then(function(mensaje){
+    console.log(mensaje);
+}).catch(function(error){
+    console.log(error);
+    
+})
+
+//--------------------------------------
+
+function obsequio(){
+    return new Promise(function(resolve, reject){
+        
+        const regalo  = true ;
+        if(regalo === true) {
+            const phone = { 
+                color: "Negro", 
+                brand:"Iphone"
+            } 
+            resolve(phone);
+     }else {
+         
+     }reject('No ganaste el regalo')
+    
+    })
+}
+  
+obsequio().then(function(mensaje){
+    console.log(mensaje);
+}).catch(function(error){
+    console.log(error);
+    
+})
+
+//--------------------Retornar otra promesa------------------
+
+
+function mostrarPhone(mensaje){
+    return new Promise(function(resolve, reject){
+        if(mensaje){
+            resolve('Obtubiste un ' + mensaje.brand);
+        }else{
+            reject('Hay un error')
+        }
+    })
+}
+
+function regalos(){
+    return new Promise(function(resolve, reject){
+        
+        const regalo  = true ;
+        if(regalo === true) {
+            const phone = { 
+                color: "Negro", 
+                brand:"Iphone"
+            } 
+            resolve(phone);
+     }else {
+         
+     }reject('No ganaste el regalo')
+    
+    })
+}
+  
+regalos().then(function(mensaje){
+    return mostrarPhone(mensaje);
+}).then(function(respuesta){
+    console.log(respuesta);
+}).catch(function(error){
+    console.log(error);
+    
+})
